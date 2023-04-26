8e3421f957e9df11704323f8a743148af6f23b91
Carsten König <Carsten.Koenig@hotmail.de>
2023-04-26 13:27:23 +0200

    fix for differences between os.machine() and process.arch
    there seems to be a difference between os.machine() and process.arch for x86/64 architecture - the first gives `x86_64` the later `x64`
    
    this is a quick work around this (or so I think)

--- download-purescript/index.js
+++ download-purescript/index.js
@@ -13,6 +13,9 @@ const supportedPlatforms = new Map([
   ['linux-x64', 'linux64'],
   ['darwin-x64', 'macos'],
   ['win32-x64', 'win64'],
+  ['linux-x86_64', 'linux64'],
+  ['darwin-x86_64', 'macos'],
+  ['win32--x86_64', 'win64'],
   ['darwin-arm64', 'macos-arm64'],
   // on Linux os.machine returns aarch64, os.arch returns arm64...
   ['linux-aarch64', 'linux-arm64'],
